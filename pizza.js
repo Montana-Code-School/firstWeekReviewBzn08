657fb10d9c81570868b98e03c6fee40279626492
kramduckner <buckner.a.mark@gmail.com>
2018-02-09 10:48:17 -0700

    Update pizza.js

--- pizza.js
+++ pizza.js
@@ -25,6 +25,10 @@ var counter = 0;
 //write a function that returns "Dominays Pizza Order no: {counter}. It is a pizza with {pepperoni} pepperonis, {onions} onion, 
 //{blackOlives} blackOlives, {sauce} sauce, and a crust of type: {type} containing {contents}"
 //use the counter to increment the counter for each order number, so that each order has a unique order number.
+//you will pass in a toppings object into your function. So you'll have call your function for each toppings object, like below
+
+//yourFunctionName(toppings1);
+//yourFunctionName(toppings2);
 
 //finally, build one more object, toppings3, populated with the same keys as toppings1 and toppings2. Give the keys values and
 //pass it into your function.
